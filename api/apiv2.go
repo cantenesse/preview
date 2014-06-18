ec663b985dd118991cdb99a7e87ccc0edf21e327
Jake Herman <jakeherman3@gmail.com>
2014-06-18 14:50:57 -0700

    Page support for getting GAs with API

--- api/apiv2.go
+++ api/apiv2.go
@@ -71,14 +71,40 @@ func NewApiV2Blueprint(
 
 func (blueprint *apiV2Blueprint) AddRoutes(p *pat.PatternServeMux) {
 	p.Put(blueprint.buildUrl("/v2/preview/"), http.HandlerFunc(blueprint.GeneratePreviewHandler))
-	p.Get(blueprint.buildUrl("/v2/preview/:id/:templateid/data/:page"), http.HandlerFunc(blueprint.PreviewGADataHandler))
-	p.Get(blueprint.buildUrl("/v2/preview/:id/:templateid"), http.HandlerFunc(blueprint.PreviewGAInfoHandler)) // Specific generated asset - /preview/123/456
+	p.Get(blueprint.buildUrl("/v2/preview/:id/:templateid/:page/data"), http.HandlerFunc(blueprint.PreviewGADataHandler))
+	p.Get(blueprint.buildUrl("/v2/preview/:id/:templateid/:page"), http.HandlerFunc(blueprint.PreviewGAInfoHandler))
+	p.Get(blueprint.buildUrl("/v2/preview/:id/:templateid"), http.HandlerFunc(blueprint.PreviewGAInfoHandler)) // Generated assets with template ID - /preview/123/456
 	p.Get(blueprint.buildUrl("/v2/preview/:id"), http.HandlerFunc(blueprint.PreviewInfoHandler))               // Get specific source asset - /preview/12345
 	p.Get(blueprint.buildUrl("/v2/preview/"), http.HandlerFunc(blueprint.PreviewQueryHandler))                 // Search - /preview/?id=1234&id=5678
 }
 
 func (blueprint *apiV2Blueprint) buildUrl(path string) string {
 	return blueprint.base + path
+}
+
+func (blueprint *apiV2Blueprint) PreviewQueryHandler(res http.ResponseWriter, req *http.Request) {
+	log.Println("Processing query request")
+
+	blueprint.previewQueriesMeter.Mark(1)
+
+	ids, hasIds := req.URL.Query()["id"]
+	log.Println(ids)
+	if !hasIds {
+		res.Header().Set("Content-Length", "0")
+		res.WriteHeader(400)
+		return
+	}
+
+	jsonData, err := blueprint.marshalSourceAssetsFromIds(ids)
+	if err != nil {
+		log.Println("Marshalling error", err)
+		res.Header().Set("Content-Length", "0")
+		res.WriteHeader(500)
+		return
+	}
+
+	res.Header().Set("Content-Length", strconv.Itoa(len(jsonData)))
+	res.Write(jsonData)
 }
 
 func (blueprint *apiV2Blueprint) PreviewInfoHandler(res http.ResponseWriter, req *http.Request) {
@@ -127,6 +153,54 @@ func (blueprint *apiV2Blueprint) PreviewGADataHandler(res http.ResponseWriter, r
 	http.NotFound(res, req)
 }
 
+func (blueprint *apiV2Blueprint) PreviewGAInfoHandler(res http.ResponseWriter, req *http.Request) {
+	blueprint.previewGAInfoRequestsMeter.Mark(1)
+	id := req.URL.Query().Get(":id")
+	templateId := req.URL.Query().Get(":templateid")
+	page := req.URL.Query().Get(":page")
+	jsonData, err := blueprint.marshalGeneratedAssets(id, templateId, page)
+	if err != nil {
+		log.Println("Marshalling error", err)
+		res.Header().Set("Content-Length", "0")
+		res.WriteHeader(500)
+		return
+	}
+	res.Header().Set("Content-Length", strconv.Itoa(len(jsonData)))
+	res.Write(jsonData)
+}
+
+func (blueprint *apiV2Blueprint) GeneratePreviewHandler(res http.ResponseWriter, req *http.Request) {
+	blueprint.generatePreviewRequestsMeter.Mark(1)
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		res.Header().Set("Content-Length", "0")
+		res.WriteHeader(400)
+		return
+	}
+	defer req.Body.Close()
+	gprs, err := newGeneratePreviewRequestV2(string(body))
+	if err != nil {
+		res.Header().Set("Content-Length", "0")
+		res.WriteHeader(400)
+		return
+	}
+
+	for _, gpr := range gprs {
+		blueprint.agentManager.CreateWorkFromTemplates(gpr.id, gpr.url, gpr.attributes, gpr.templateIds)
+	}
+
+	target := blueprint.buildUrl("/v2/preview/")
+	for idx, gpr := range gprs {
+		if idx != 0 {
+			target += "&"
+		} else {
+			target += "?"
+		}
+		target += "id=" + gpr.id
+	}
+	http.Redirect(res, req, target, 303)
+}
+
 func (blueprint *apiV2Blueprint) splitS3Url(url string) (string, string) {
 	usableData := url[5:]
 	// NKG: The url will have the following format: `s3://[bucket][path]`
@@ -171,54 +245,6 @@ func (blueprint *apiV2Blueprint) getAsset(fileId, templateId, page string) (asse
 	return assetAction404, ""
 }
 
-func (blueprint *apiV2Blueprint) PreviewGAInfoHandler(res http.ResponseWriter, req *http.Request) {
-	blueprint.previewGAInfoRequestsMeter.Mark(1)
-	id := req.URL.Query().Get(":id")
-	templateId := req.URL.Query().Get(":templateid")
-	jsonData, err := blueprint.marshalGeneratedAsset(id, templateId)
-	if err != nil {
-		log.Println("Marshalling error", err)
-		res.Header().Set("Content-Length", "0")
-		res.WriteHeader(500)
-		return
-	}
-	res.Header().Set("Content-Length", strconv.Itoa(len(jsonData)))
-	res.Write(jsonData)
-}
-
-func (blueprint *apiV2Blueprint) PreviewAttributeHandler(res http.ResponseWriter, req *http.Request) {
-	//blueprint.previewAttributeRequestsMeter.Mark(1)
-	//id := req.URL.Query().Get(":id")
-}
-
-func (blueprint *apiV2Blueprint) GeneratePreviewHandler(res http.ResponseWriter, req *http.Request) {
-	blueprint.generatePreviewRequestsMeter.Mark(1)
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		res.Header().Set("Content-Length", "0")
-		res.WriteHeader(400)
-		return
-	}
-	defer req.Body.Close()
-	gprs, err := newGeneratePreviewRequestV2(string(body))
-	if err != nil {
-		res.Header().Set("Content-Length", "0")
-		res.WriteHeader(400)
-		return
-	}
-	blueprint.handleGeneratePreviewRequest(gprs)
-	target := blueprint.buildUrl("/v2/preview/")
-	for idx, gpr := range gprs {
-		if idx != 0 {
-			target += "&"
-		} else {
-			target += "?"
-		}
-		target += "id=" + gpr.id
-	}
-	http.Redirect(res, req, target, 303)
-}
-
 type sourceAssetView struct {
 	SourceAssets []extendedSourcedAsset `json:"sourceAssets"`
 }
@@ -232,31 +258,6 @@ type generatedAssetView struct {
 	GeneratedAssetId string             `json:"generatedAssetId"`
 	Location         string             `json:"location"`
 	Attributes       []common.Attribute `json:"attributes"`
-}
-
-func (blueprint *apiV2Blueprint) PreviewQueryHandler(res http.ResponseWriter, req *http.Request) {
-	log.Println("Processing query request")
-
-	blueprint.previewQueriesMeter.Mark(1)
-
-	ids, hasIds := req.URL.Query()["id"]
-	log.Println(ids)
-	if !hasIds {
-		res.Header().Set("Content-Length", "0")
-		res.WriteHeader(400)
-		return
-	}
-
-	jsonData, err := blueprint.marshalSourceAssetsFromIds(ids)
-	if err != nil {
-		log.Println("Marshalling error", err)
-		res.Header().Set("Content-Length", "0")
-		res.WriteHeader(500)
-		return
-	}
-
-	res.Header().Set("Content-Length", strconv.Itoa(len(jsonData)))
-	res.Write(jsonData)
 }
 
 func (blueprint *apiV2Blueprint) marshalSourceAssetsFromIds(ids []string) ([]byte, error) {
@@ -288,7 +289,7 @@ func (blueprint *apiV2Blueprint) marshalSourceAssetsFromIds(ids []string) ([]byt
 	return jsonData, nil
 }
 
-func (blueprint *apiV2Blueprint) marshalGeneratedAsset(said string, templateId string) ([]byte, error) {
+func (blueprint *apiV2Blueprint) marshalGeneratedAssets(said, templateId, page string) ([]byte, error) {
 	gas, err := blueprint.gasm.FindBySourceAssetId(said)
 	if err != nil {
 		return nil, err
@@ -297,7 +298,18 @@ func (blueprint *apiV2Blueprint) marshalGeneratedAsset(said string, templateId s
 	var arr []*common.GeneratedAsset
 	for _, g := range gas {
 		if g.TemplateId == templateId {
-			arr = append(arr, g)
+			if len(page) > 0 {
+				gPage, err := common.GetFirstAttribute(g, common.GeneratedAssetAttributePage)
+				if err != nil {
+					gPage = "0"
+				}
+				if page == gPage {
+					arr = append(arr, g)
+					break
+				}
+			} else {
+				arr = append(arr, g)
+			}
 		}
 	}
 
@@ -336,10 +348,4 @@ func newGeneratePreviewRequestV2(body string) ([]*generatePreviewRequestV2, erro
 		gprs = append(gprs, gpr)
 	}
 	return gprs, nil
-}
-
-func (blueprint *apiV2Blueprint) handleGeneratePreviewRequest(gprs []*generatePreviewRequestV2) {
-	for _, gpr := range gprs {
-		blueprint.agentManager.CreateWorkFromTemplates(gpr.id, gpr.url, gpr.attributes, gpr.templateIds)
-	}
 }
